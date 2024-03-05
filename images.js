e21312be5a962ef1b83518eab07c2ea1ec580f77
Santiago <santiagora1987@gmail.com>
2024-03-05 12:23:30 -0300

    imagenes del orto andando

--- images.js
+++ images.js
@@ -1,57 +1,46 @@
-//getAllImagesID()
+import fs from 'node:fs'
+import FormData from 'form-data';
+import axios from 'axios';
+
 const getImage = async (id) => {
   try {
-    /*fetch(`https://dydsoft.com/imagina/portal/ver_foto.php?id=432`)
-      .then((response) => response.blob())
-      .then((blob) => {
-        const objectURL = URL.createObjectURL(blob);
-        console.log(objectURL);
-        //image.src = objectURL;
-      });*/
-
-    let response = await fetch(
-      `https://dydsoft.com/imagina/portal/ver_foto.php?id=432`
-    );
-    let blob = await response.blob("image/jpg");
-    //const objectURL = URL.createObjectURL(blob);
-
-    /*let response = await fetch(
-      "https://dydsoft.com/imagina/portal/ver_foto.php?id=432"
-    );
-    let reader = response.body.getReader();
-    let read = await reader.read();
-    let val = btoa(String.fromCharCode.apply(null, read.value));
-    let base64 = "data:image/png;base64," + val;
-
-    console.log(base64);*/
 
     const formData = new FormData();
-    formData.append("image", blob);
+    const filePath = `./downloads/432.jpg`;
+    //const stats = fs.statSync(filePath);
+    //const fileSizeInBytes = stats.size;
 
-    const res = await fetch("https://libreria-test.net/api/images/products/1", {
+    const imageBuffer = fs.readFileSync(filePath)
+
+    const fileStream = fs.createReadStream(filePath);
+
+    formData.append("image", imageBuffer, "432.jpg");
+    const leng = formData.getLengthSync()
+
+    /*const res = await fetch("https://libreria-test.net/api/images/products/1", {
       method: "POST",
       headers: {
         Authorization: "Basic NVJYNzYxSTNBUDJTRkxSNTZDNUM4REFUU1RKRzFFVEw6",
+        "Content-Length": leng,
+        "Content-Type": "application/json"
       },
       body: formData,
     });
     console.log(res.status);
-    console.log(await res.text());
+    console.log(await res.text());*/
 
-    /*fetch("https://dydsoft.com/imagina/portal/ver_foto.php?id=432")
-      .then((response) => response.body.getReader())
-      .then((response) => response.read())
-      .then((array) => btoa(String.fromCharCode.apply(null, array.value)))
-      .then((partialBase64) => "data:image/png;base64," + partialBase64)
-      .then((fullBase64) => {
-        console.log(fullBase64);
+    axios.post("https://libreria-test.net/api/images/products/1", formData, {
+      headers: {
+        Authorization: "Basic NVJYNzYxSTNBUDJTRkxSNTZDNUM4REFUU1RKRzFFVEw6",
+        "Content-Length": leng,
+      }
+    }).then((response) => {
+      console.log(response.status);
+    })
+      .catch((error) => {
+        console.error(error.response.data);
+      });
 
-        const formData = new FormData();
-        formData.append("image", fullBase64);
-
-
-
-      });*/
   } catch (err) {
     console.log("Error: ", err);
     return undefined;
