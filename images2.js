a1d1912144b5b401cc358956ebee964fbb77887a
Santiago Ramos Alvarez <Santiago@Santiago-Mac.local>
2024-03-11 20:48:57 -0300

    arreglado stock?

--- images2.js
+++ images2.js
@@ -1,20 +1,22 @@
-import FormData from 'form-data';
-import axios from 'axios';
-import fs from 'node:fs'
+import FormData from "form-data";
+import axios from "axios";
+import fs from "node:fs";
 
 const formData = new FormData();
 const filePath = `./imagenes/432.jpg`;
 
-const imageBuffer = fs.readFileSync(filePath)
+const imageBuffer = fs.readFileSync(filePath);
 
 formData.append("image", imageBuffer, "image.jpg");
-const leng = formData.getLengthSync()
+const leng = formData.getLengthSync();
 
-await axios.post(`https://libreria-test.net/api/images/products/1`, formData, {
+axios
+  .post(`https://libreria-test.net/api/images/products/1`, formData, {
     headers: {
-        Authorization: "Basic NVJYNzYxSTNBUDJTRkxSNTZDNUM4REFUU1RKRzFFVEw6",
-        "Content-Length": leng,
-    }
-}).then(res => console.log(res.status.data))
-    .catch(err => console.log(err.data))
+      Authorization: "Basic NVJYNzYxSTNBUDJTRkxSNTZDNUM4REFUU1RKRzFFVEw6",
+      "Content-Length": leng,
+    },
+  })
+  .then((res) => console.log(res.status.data))
+  .catch((err) => console.log(err.data));
 //console.log(432, "result.status.data", i);
---
+++ stocksManagment/stockTest.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+let idstock = 13050;
+let qty = 2;
+let idprod = 3;
+
+/*let result = await fetch(
+  `https://libreria-test.net/api/stock_availables/${idstock}`,
+  {
+    method: "PUT",
+    headers: {
+      Authorization: "Basic NVJYNzYxSTNBUDJTRkxSNTZDNUM4REFUU1RKRzFFVEw6",
+    },
+    body: `<?xml version="1.0" encoding="UTF-8"?>
+                <prestashop xmlns:xlink="http://www.w3.org/1999/xlink">
+                <stock_available>
+                    <id><![CDATA[${idstock}]]></id>
+                    <quantity><![CDATA[${qty}]]></quantity>
+                    <id_product><![CDATA[${idprod}]]></id_product>
+                    <depends_on_stock>0</depends_on_stock>
+                    <out_of_stock>0</out_of_stock>
+                    <id_shop>1</id_shop>
+                </stock_available>
+            </prestashop>`,
+  }
+);
+
+console.log(result.status);*/
+let body = `<?xml version="1.0" encoding="UTF-8"?>
+                <prestashop xmlns:xlink="http://www.w3.org/1999/xlink">
+                <stock_available>
+                    <id><![CDATA[${idstock}]]></id>
+                    <quantity><![CDATA[${qty}]]></quantity>
+                    <id_product><![CDATA[${idprod}]]></id_product>
+                    <depends_on_stock>0</depends_on_stock>
+                    <out_of_stock>0</out_of_stock>
+                    <id_shop>1</id_shop>
+                </stock_available>
+            </prestashop>`;
+
+await axios
+  .patch(`https://libreria-test.net/api/stock_availables/${idstock}`, body, {
+    headers: {
+      Authorization: "Basic NVJYNzYxSTNBUDJTRkxSNTZDNUM4REFUU1RKRzFFVEw6",
+    },
+  })
+  .then((res) => console.log(res.data))
+  .catch((err) => console.error(err));
