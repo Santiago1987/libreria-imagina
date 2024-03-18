47f71c89c26010a17afc661ad7e18057b9d20779
Santiago Ramos Alvarez <Santiago@Santiago-Mac.local>
2024-03-18 20:58:32 -0300

    prueba de archivo test

--- test.js
+++ test.js
@@ -1,36 +1,50 @@
-const getImage = async (id) => {
-  try {
-    let result = await fetch(`https://dydsoft.com/imagina/portal/mostrar_foto.php?id=${id}`, {
-      method: 'GET'
-    })
-
-    return result.blob()
-  } catch (err) {
-    console.log("Error: ", err)
-    return undefined
+import md5 from "./md5.js";
+async function getProd() {
+  let response = await fetch(
+    "https://dydsoft.com/imagina/webservice.php?operation=getchallenge&username=integracion",
+    {
+      method: "GET",
+    }
+  );
+  if (!response.ok) {
+    throw new Error("Error en la api");
   }
 
-}
+  let res = await response.json();
+  let { result } = res;
+  let cript = md5(result.token + "fUn6nRDdKksNaFmF");
 
-async function saveImage(prodID) {
-  try {
-    let image = getImage(prodID)
-    if (!image) return
+  let response2 = await fetch(
+    "https://dydsoft.com/imagina/webservice.php?operation=login&username=integracion&accessKey=" +
+      cript,
+    {
+      method: "GET",
+    }
+  );
 
-    const formData = new FormData()
-    formData.append("image", image)
-
-
-    let result = await fetch(`https://libreria-test.net/api/images/products/${prodID}`, {
-      method: 'POST'
-    })
-
-  } catch (err) {
-    console.log("Error: ", err)
-    return undefined
+  if (!response2.ok) {
+    throw new Error("Error en la api");
   }
+
+  let res2 = await response2.json();
+  let result2 = res2.result;
+
+  let response3 = await fetch(
+    `https://dydsoft.com/imagina/webservice.php?operation=query&elementType=Products&busqueda0=${364}&campo0=crmid&sessionName=` +
+      result2.sessionName,
+    {
+      method: "GET",
+    }
+  );
+
+  if (!response3.ok) {
+    throw new Error("Error en la api");
+  }
+
+  let res3 = await response3.json();
+  let result3 = res3.result;
+
+  return result3;
 }
 
-const formData = new FormData()
-formData.append("image", getImage(1))
-
+console.log(await getProd());
