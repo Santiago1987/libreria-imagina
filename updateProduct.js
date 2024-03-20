6c997964b4d9967a8c0390a7823968f06b3aa259
Santiago Ramos Alvarez <Santiago@Santiago-Mac.local>
2024-03-20 20:50:03 -0300

    crom script finished

--- updateProduct.js
+++ updateProduct.js
@@ -2,7 +2,7 @@ try {
   // LISTA DE PRODUCTS YA GUARDADOS [{id, reference}]
   let savedProducts = await savedProductList();
 
-  //ISTA DE PRODUCTS PARA UPDATE
+  //LISTA DE PRODUCTS PARA UPDATE
   let updProductList = await productsUPD();
 
   //LISTA DE LOS STOCK GUARDADOS CON LOS IDS
@@ -17,6 +17,8 @@ try {
   //RECORRO LOS PRODUCTOS PARA UPDATE
   for (let prod of updProductList) {
     let { crmid } = prod;
+
+    //CHEQUEO SI EL PRODUCTO YA EXISTE EN LA BD
     let isSave = savedProducts.find((el) => el.reference === crmid);
 
     //SI EL PRODUCTO YA EXISTE SE ACTUALIZA STOCK
@@ -39,122 +41,122 @@ try {
             reference
         );
       }
-      //SI EL PRODUCTO NO EXISTE SE INTENTA GUARDAR
-      if (!stock) {
-        let {
-          crmid,
-          qtyinstock,
-          cf_1372,
-          unit_price,
-          description,
-          productname,
-          weight,
-          cf_1374,
-          cf_1376,
-          cf_1375,
-          cf_1395,
-          cf_1394,
-        } = prod;
+    }
+    //-----------------------------------------------------------------------------------
+    //SI EL PRODUCTO NO EXISTE SE INTENTA GUARDAR
+    if (!isSave) {
+      let {
+        crmid,
+        qtyinstock,
+        cf_1372,
+        unit_price,
+        description,
+        productname,
+        weight,
+        cf_1374,
+        cf_1376,
+        cf_1375,
+        cf_1395,
+        cf_1394,
+        fotosid,
+      } = prod;
 
-        //OBTENGO EL ID DEL PROVEEDOR
-        let manuid = await getManufacturerID(cf_1374);
-        manuid ??= 1; // valor por defecto en caso de que no ecuentre nada
+      //OBTENGO EL ID DEL PROVEEDOR
+      let manuid = await getManufacturerID(cf_1374);
+      manuid ??= 1; // valor por defecto en caso de que no ecuentre nada
 
-        //LISTA DE LAS FEATURES DEL PRODUCTO
-        let prodFeat = destructureFeat(cf_1394);
+      //LISTA DE LAS FEATURES DEL PRODUCTO
+      let prodFeat = destructureFeat(cf_1394);
 
-        let featureXML = "";
-        //ARMO EL XML DE LAS FEATURES CON SUS VALORES
-        if (prodFeat) {
-          // RECORRO LAS FEATURES DEL PRODUCTO
-          for (let featName of Object.keys(prodFeat)) {
-            let featval = prodFeat[featName];
+      let featureXML = "";
+      //ARMO EL XML DE LAS FEATURES CON SUS VALORES
+      if (prodFeat) {
+        // RECORRO LAS FEATURES DEL PRODUCTO
+        for (let featName of Object.keys(prodFeat)) {
+          let featval = prodFeat[featName];
 
-            //BUSCO EN LA TABLA DE FEATURES EL ID DE LA FEATURE Y EL ID DEL VALOR
-            let featureSQL = featuresTable.find((el) => el.name === featName);
+          //BUSCO EN LA TABLA DE FEATURES EL ID DE LA FEATURE Y EL ID DEL VALOR
+          let featureSQL = featuresTable.find((el) => el.name === featName);
 
-            if (featureSQL) {
-              let { id: idFeat, values } = featureSQL;
+          if (featureSQL) {
+            let { id: idFeat, values } = featureSQL;
 
-              //BUSCO EL ID DE LOS VALUES
-              let featureValSQL = values.find((el) => el.value === featval);
-              if (!featureValSQL)
-                featureValSQL = values.find((el) => el.value.includes(featval));
+            //BUSCO EL ID DE LOS VALUES
+            let featureValSQL = values.find((el) => el.value === featval);
+            if (!featureValSQL)
+              featureValSQL = values.find((el) => el.value.includes(featval));
 
-              if (featureValSQL) {
-                let { id: idvalue } = featureValSQL;
+            if (featureValSQL) {
+              let { id: idvalue } = featureValSQL;
 
-                // UNA VEZ OBTENIDOS LOS 2 IDS ARMO EL XML DE LAS FEATURES
-                featureXML =
-                  featureXML +
-                  `<product_feature>
+              // UNA VEZ OBTENIDOS LOS 2 IDS ARMO EL XML DE LAS FEATURES
+              featureXML =
+                featureXML +
+                `<product_feature>
               <id><![CDATA[${idFeat}]]></id>
               <id_feature_value><![CDATA[${idvalue}]]></id_feature_value>
               </product_feature>`;
-              }
             }
           }
         }
+      }
 
-        // CATEGORIES
-        //ARMO UNA ARRAY CON LAS JERARQUIAS
-        let catNames = [cf_1376, cf_1375];
-        if (
-          cf_1395 !== "" &&
-          cf_1395 !== " " &&
-          cf_1395 !== undefined &&
-          !cf_1395.includes("---")
-        )
-          catNames[2] = cf_1395;
+      // CATEGORIES
+      //ARMO UNA ARRAY CON LAS JERARQUIAS
+      let catNames = [cf_1376, cf_1375];
+      if (
+        cf_1395 !== "" &&
+        cf_1395 !== " " &&
+        cf_1395 !== undefined &&
+        !cf_1395.includes("---")
+      )
+        catNames[2] = cf_1395;
 
-        // CONS LOS NOMBRES BUSCO LOS IDS DE LA BD SQL
-        let cat = catTableSQL.find((el) => {
-          let { catlist } = el;
-          return (
-            catlist[0] == catNames[0] &&
-            catlist[1] == catNames[1] &&
-            catlist[2] == catNames[2]
-          );
-        });
+      // CONS LOS NOMBRES BUSCO LOS IDS DE LA BD SQL
+      let cat = catTableSQL.find((el) => {
+        let { catlist } = el;
+        return (
+          catlist[0] == catNames[0] &&
+          catlist[1] == catNames[1] &&
+          catlist[2] == catNames[2]
+        );
+      });
 
-        if (cat) {
-          // SI OBETENGO AL CATEGORIA GUARDO EL PRODUCTO
-          let categoriID = cat.idcat;
+      // SI OBETENGO AL CATEGORIA GUARDO EL PRODUCTO
+      if (cat) {
+        let categoriID = cat.idcat;
 
-          //ACOMODO DE LOS DATOS
+        //ACOMODO DE LOS DATOS
+        productname = htmlEntities(productname).replaceAll(";", "");
+        description = htmlEntities(description)
+          .replaceAll(";", "")
+          .replaceAll("<", "'")
+          .replaceAll(">", "'");
 
-          productname = htmlEntities(productname).replaceAll(";", "");
-          description = htmlEntities(description)
-            .replaceAll(";", "")
-            .replaceAll("<", "'")
-            .replaceAll(">", "'");
+        if (!Number(cf_1372)) cf_1372 = ""; // EAN QUE SON PALABRAS
+        if (!Number(weight)) weight = "";
 
-          cf_1375 = 2; //ESTAN MAL LAS CATEGORIAS
+        let title = productname //acá y en description en vez de reemplazar los caracteres podés aplicar la función decodeHtml(param)
+          ? productname
+              .toLowerCase()
+              .split(" ")
+              .map((el) => {
+                if (!el[0]) return el;
+                return el[0].toUpperCase() + el.slice(1);
+              })
+              .toString()
+              .replaceAll(",", " ")
+          : "";
 
-          if (!Number(cf_1372)) cf_1372 = ""; // EAN QUE SON PALABRAS
-          if (!Number(weight)) weight = "";
+        let metaDescription = description.slice(0, 500);
+        cf_1372 = cf_1372.length > 13 ? "" : cf_1372;
 
-          let title = productname //acá y en description en vez de reemplazar los caracteres podés aplicar la función decodeHtml(param)
-            ? productname
-                .toLowerCase()
-                .split(" ")
-                .map((el) => {
-                  if (!el[0]) return el;
-                  return el[0].toUpperCase() + el.slice(1);
-                })
-                .toString()
-                .replaceAll(",", " ")
-            : "";
-
-          let metaDescription = description.slice(0, 500);
-          cf_1372 = cf_1372.length > 13 ? "" : cf_1372;
-
-          let saveProdXML = `<?xml version="1.0" encoding="UTF-8"?>
+        let saveProdXML = `<?xml version="1.0" encoding="UTF-8"?>
             <prestashop xmlns:xlink="http://www.w3.org/1999/xlink">
             <product>
               <id_manufacturer><![CDATA[${manuid}]]></id_manufacturer>
               <id_supplier></id_supplier>
-              <id_category_default><![CDATA[${cf_1375}]]></id_category_default>
+              <id_category_default><![CDATA[${categoriID}]]></id_category_default>
               <new>1></new>
               <id_default_combination></id_default_combination>
               <id_tax_rules_group></id_tax_rules_group>
@@ -164,7 +166,7 @@ try {
               <supplier_reference></supplier_reference>
               <ean13><![CDATA[${cf_1372}]]></ean13>
               <state>1</state>
-              <product_type></product_type>
+              <product_type><![CDATA[standard]]></product_type>
               <price><![CDATA[${unit_price}]]></price>
               <unit_price><![CDATA[${unit_price}]]></unit_price>
               <active>1</active>
@@ -207,20 +209,46 @@ try {
             </product>
           </prestashop>`;
 
-          let result = await fetch("https://libreria-test.net/api/products", {
-            method: "POST",
-            headers: {
-              Authorization:
-                "Basic NVJYNzYxSTNBUDJTRkxSNTZDNUM4REFUU1RKRzFFVEw6",
-            },
-            body: saveProdXML,
-          });
+        let result = await fetch("https://libreria-test.net/api/products", {
+          method: "POST",
+          headers: {
+            Authorization: "Basic NVJYNzYxSTNBUDJTRkxSNTZDNUM4REFUU1RKRzFFVEw6",
+          },
+          body: saveProdXML,
+        });
 
-          console.log(i + " " + crmid + " " + result.status);
-          if (result.status < 200 || result.status > 299) {
-            console.log("Ultimo id " + i + " product id: " + crmid);
-            console.log("Razon " + (await result.text()));
-            return;
+        console.log(i + " " + crmid + " " + result.status);
+        if (result.status < 200 || result.status > 299) {
+          console.log("Ultimo id " + i + " product id: " + crmid);
+          console.log("Razon " + (await result.text()));
+          return;
+        }
+
+        //--------------------------------------STOCK--------------------------------------
+        // SI EL GUARDADO FUE BIEN ACTUALIZO STOCK
+        //OBTENGO EL ID DEL PRODUCTO NUEVO
+        let idprodNew = await getProductID(crmid);
+        //ID DEL STOCK
+        let idProdStock = await getStockProductID(idprodNew);
+        //GUARDADO DE STOCK
+        let responseStock = await saveStockComplete(
+          idProdStock,
+          +qtyinstock,
+          idprodNew
+        );
+
+        if (!responseStock.ok)
+          console.error("Producto: ", idprodNew, crmid, "stock no actualizado");
+
+        //--------------------------------------IMAGENES--------------------------------------
+        if (!fotosid) {
+          await saveImages(crmid, idprodNew, false);
+        }
+
+        if (fotosid) {
+          let fotolist = fotosid.split(",");
+          for (let el of fotolist) {
+            await saveImages(el, idprodNew, true);
           }
         }
       }
@@ -299,6 +327,7 @@ async function productsUPD() {
   return result3;
 }
 
+//FUNCION QUE ACTUALIZA STOCK DE UN PRODUCTO
 async function updStock(idstock, qty) {
   let result = await fetch(
     `https://libreria-test.net/api/stock_availables/${idstock}`,
@@ -320,6 +349,32 @@ async function updStock(idstock, qty) {
   return result;
 }
 
+//GUARDADO COMPLETO DE STOCK
+async function saveStockComplete(idstock, qty, idprod) {
+  let result = await fetch(
+    `https://libreria-test.net/api/stock_availables/${idstock}`,
+    {
+      method: "PATCH",
+      headers: {
+        Authorization: "Basic NVJYNzYxSTNBUDJTRkxSNTZDNUM4REFUU1RKRzFFVEw6",
+      },
+      body: `<?xml version="1.0" encoding="UTF-8"?>
+                <prestashop xmlns:xlink="http://www.w3.org/1999/xlink">
+                <stock_available>
+                    <id><![CDATA[${idstock}]]></id>
+                    <quantity><![CDATA[${qty}]]></quantity>
+                    <id_product><![CDATA[${idprod}]]></id_product>
+                    <depends_on_stock><![CDATA[0]]></depends_on_stock>
+                    <out_of_stock><![CDATA[0]]></out_of_stock>
+                    <id_shop><![CDATA[1]]></id_shop>
+                    <id_product_attribute><![CDATA[0]]></id_product_attribute>
+                </stock_available>
+            </prestashop>`,
+    }
+  );
+  return result;
+}
+
 // STOCK ID TABLE
 async function getStockIdTable() {
   let result = await fetch(
@@ -337,6 +392,46 @@ async function getStockIdTable() {
   let { stock_availables } = data;
 
   return stock_availables;
+}
+
+//ID DE UN PRODUCTO EN ESPECIFICO
+async function getProductID(ref) {
+  let result = await fetch(
+    `https://libreria-test.net/api/products?display=[id,reference]&filter[reference]=${ref}&output_format=JSON`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: "Basic NVJYNzYxSTNBUDJTRkxSNTZDNUM4REFUU1RKRzFFVEw6",
+      },
+    }
+  );
+
+  let data = await result.json();
+
+  let { products } = data;
+  let [id] = products[0];
+
+  return products;
+}
+
+//ID DEL STOCK DE UN PRODUCTO EN ESPECIFICO
+async function getStockProductID(idprod) {
+  let result = await fetch(
+    `https://libreria-test.net/api/stock_availables?display=[id]&filter[id_product]=${idprod}&output_format=JSON`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: "Basic NVJYNzYxSTNBUDJTRkxSNTZDNUM4REFUU1RKRzFFVEw6",
+      },
+    }
+  );
+
+  let data = await result.json();
+
+  let { stock_availables } = data;
+  let { id } = stock_availables[0];
+
+  return id;
 }
 
 //LISTA DE CATEGORIAS DE LA BD SQL
@@ -516,6 +611,49 @@ function destructureFeat(feat) {
     }
   }
   return result;
+}
+
+//GUARDADO DE IMAGENES
+async function saveImages(ref, idProd, isFotoid = false) {
+  let url = "";
+
+  if (!isFotoid) {
+    url = `https://dydsoft.com/imagina/portal/ver_foto.php?id=${ref}`;
+  }
+  if (isFotoid) {
+    url = `https://dydsoft.com/imagina/portal/mostrar_foto.php?id=${ref}`;
+  }
+
+  let result = await fetch(url);
+  let blob = await result.blob();
+  blob.name = reference + ".jpeg";
+  blob.lastModified = new Date();
+
+  const imgfile = new File([blob], reference + ".jpeg", {
+    type: blob.type,
+  });
+  const formData = new FormData();
+  formData.append("image", imgfile);
+  //console.log("data", data);
+
+  //la unica manera que funciono de conseguir el puto size
+  const response = new Response(formData);
+  let blb = await response.blob();
+  let size = blb.size;
+
+  fetch(`https://libreria-test.net/api/images/products/${idProd}`, {
+    method: "POST",
+    headers: {
+      Authorization: "Basic NVJYNzYxSTNBUDJTRkxSNTZDNUM4REFUU1RKRzFFVEw6",
+      "Content-Length": size,
+    },
+    body: formData,
+  })
+    .then((res) => {
+      console.log(res.status);
+      res.text().then((tx) => console.log(tx));
+    })
+    .catch((err) => console.log(err));
 }
 
 function htmlEntities(str) {
